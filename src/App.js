b027edaf63ad853d5d6bef7fa9f3b98eeece820d
maosan132 <maosan132@gmail.com>
2021-08-30 15:46:14 -0500

    Fix linters for App and index

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={} className="App-logo" alt="logo" />
+        <img src={null} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
         <a
           className="App-link"
-          href=""
+          href="www.home.com"
           target="_blank"
           rel="noopener noreferrer"
         >
