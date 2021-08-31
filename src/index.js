660ec8b8f1567bdeff869c9bf8c1c95ebd44d90e
maosan132 <maosan132@gmail.com>
2021-08-30 23:10:13 -0500

    Implement reducers, add an initial state

--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const books = [
+  {
+    title: 'The Design of EveryDay Things',
+    author: 'Don Norman',
+    id: Math.floor(Math.random() * 1000),
+    category: 'Design'
+  },
+  {
+    title: 'The Most Human Human',
+    author: 'Brian Christian',
+    id: Math.floor(Math.random() * 1000),
+    category: 'Psychology'
+  },
+  {
+    title: 'Clean Code',
+    author: 'Robert C. Martin',
+    id: Math.floor(Math.random() * 1000),
+    category: 'Software Practices'
+  }
+]
+
+const initialState = {books}
 
 ReactDOM.render(
   <Provider store={store}>
