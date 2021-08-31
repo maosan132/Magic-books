2c7366cf46a857feb3f2686a8bd37666113e033a
maosan132 <maosan132@gmail.com>
2021-08-30 22:24:08 -0500

    Finish implementation of  BookList comp

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,8 +1,31 @@
 const BookList = () => {
+  const books = useSelector(state => state.books);
   return (
     <div>
       <h1>Books List</h1>
-      <BookListItem />
+      <table>
+        <thead>
+          <tr>
+            <th>Book ID</th>
+            <th>Title</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            books.map(b => (
+              <Book
+                key={b.id}
+                bookId={b.id}
+                title={b.title}
+                category={b.category}
+              />
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
+
+export default BookList;
